df4a4f41ffdef1f2fc81a5411eb896ca27af9aa6
SanoopAnanth <91669722+SanoopAnanth@users.noreply.github.com>
2022-05-13 13:45:07 +0000

    where is maximum

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,16 +1,30 @@
 # Loops & Iterators
-
 largest = None
 smallest = None
-
 while True:
-    num = input("Enter a number? ")
-
+    num = input("Enter a number: ")
     if num == "done":
         break
-
-    # ...
-
     print(num)
-
+    try:
+      flsv=float(num)
+    except:
+      print('Invalid input')
+    for i in num:
+       if smallest is None:
+            smallest=num
+       elif num<smallest:
+            smallest=num
+    for i in num:
+        if largest is None:
+            largest=num
+        elif num>largest:
+            largest=num
+  
+      
+print("Minimum",smallest)
 print("Maximum", largest)
+
+      
+      
+    
