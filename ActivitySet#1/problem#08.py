9bca5754c18e7bcc2a24fb224a615bac408a8510
SanoopAnanth <91669722+SanoopAnanth@users.noreply.github.com>
2022-05-14 11:49:03 +0000

    attention needed!

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -3,12 +3,14 @@
 #filename = "dataset/mbox-short.txt"
 fname=input('enter file name: ')
 fopen=open(fname)
-print(fopen)
-count1=0
-count2=0
+#print(fopen)
+count1=0.0
+count2=0.0
 for line in fopen:
   count1=count1+1
 for line in fopen:
   if line=='X-DSPAM-Confidence:    0.8475':
     count2=count2+1
-print('average spam confidence: ',count1/count2)
+print('count1',count1)
+print('count2',count2)
+print('average spam confidence: ',count1+count2/2)
