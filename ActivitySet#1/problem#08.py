bdfea729672954ac589adf143148a35c5f48d850
SanoopAnanth <91669722+SanoopAnanth@users.noreply.github.com>
2022-05-14 11:44:02 +0000

    need a look

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,14 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+#filename = "dataset/mbox-short.txt"
+fname=input('enter file name: ')
+fopen=open(fname)
+print(fopen)
+count1=0
+count2=0
+for line in fopen:
+  count1=count1+1
+for line in fopen:
+  if line=='X-DSPAM-Confidence:    0.8475':
+    count2=count2+1
+print('average spam confidence: ',count1/count2)
