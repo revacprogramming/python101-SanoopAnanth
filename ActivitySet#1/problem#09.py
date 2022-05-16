4cb636660f7dc4a4006e8c482db9b1843fa72286
SanoopAnanth <91669722+SanoopAnanth@users.noreply.github.com>
2022-05-16 08:30:32 +0000

    done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,17 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+#filename = "dataset/romeo.txt"
+file=input('enter file name: ')
+fh=open(file)
+t=[]
+s=[]
+for line in fh:
+    str1=line.split()
+    t.append(str1)
+for ele in t:
+    if(ele in s):
+        continue
+    else:
+        s.append(ele)
+        
+print(s)
