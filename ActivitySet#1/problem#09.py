6feb5062abab2fa48a389d3dbcfa139bb7eea82c
SanoopAnanth <91669722+SanoopAnanth@users.noreply.github.com>
2022-05-16 08:44:43 +0000

    done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -4,14 +4,12 @@
 file=input('enter file name: ')
 fh=open(file)
 t=[]
-s=[]
 for line in fh:
-    str1=line.split()
-    t.append(str1)
-for ele in t:
-    if(ele in s):
-        continue
-    else:
-        s.append(ele)
-        
-print(s)
+    str1=line.rstrip().split()
+    for ele in str1:
+        if ele not in t:
+            t.append(ele)
+        else:
+            continue
+t.sort()
+print(t)
